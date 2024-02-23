57eff86afaf5aa4a1903c38d88b4c43e2f14c9f4
Ryan Tong <baotongxuan@gmail.com>
2024-02-23 09:33:06 +0000

    ref: medusa to my pham xuanthuy

--- xuanthuyshop-storefront/src/modules/layout/components/side-menu/index.tsx
+++ xuanthuyshop-storefront/src/modules/layout/components/side-menu/index.tsx
@@ -84,7 +84,7 @@ const SideMenu = ({ regions }: { regions: Region[] | null }) => {
                         />
                       </div>
                       <Text className="flex justify-between txt-compact-small">
-                        © {new Date().getFullYear()} Medusa Store. All rights
+                        © {new Date().getFullYear()} Mỹ Phẩm Xuân Thùy. All rights
                         reserved.
                       </Text>
                     </div>
