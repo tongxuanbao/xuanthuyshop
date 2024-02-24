bf45f73e020e501861512ceb5777e03bdf9ef28d
Ryan Tong <baotongxuan@gmail.com>
2024-02-24 08:05:45 +0000

    feat: add s3 bucket

--- xuanthuyshop/medusa-config.js
+++ xuanthuyshop/medusa-config.js
@@ -61,6 +61,21 @@ const plugins = [
         open: process.env.OPEN_BROWSER !== "false",
       },
     },
+  }, 
+  {
+    resolve: `medusa-file-s3`,
+    options: {
+        s3_url: process.env.S3_URL,
+        bucket: process.env.S3_BUCKET,
+        region: process.env.S3_REGION,
+        access_key_id: process.env.S3_ACCESS_KEY_ID,
+        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+        // cache_control: process.env.S3_CACHE_CONTROL,
+        // optional
+        // download_file_duration:
+        //   process.env.S3_DOWNLOAD_FILE_DURATION,
+        // prefix: process.env.S3_PREFIX,
+    },
   },
 ];
 
