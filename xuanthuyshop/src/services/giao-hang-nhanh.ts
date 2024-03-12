a672aaf471a771e1c93726794a942a6cf09e3bb1
Ryan Tong <baotongxuan@gmail.com>
2024-03-12 18:19:57 +1100

    Added giao hang nhanh shipping

--- xuanthuyshop/src/services/giao-hang-nhanh.ts
+++ xuanthuyshop/src/services/giao-hang-nhanh.ts
@@ -8,13 +8,67 @@ import {
     Order,
 } from "@medusajs/medusa";
 import { CreateReturnType } from "@medusajs/medusa/dist/types/fulfillment-provider";
+import axios from "axios";
+
+const GIAO_HANG_HANH_API_TOKEN = process.env.GIAO_HANG_NHANH_API_TOKEN ?? "";
+const GIAO_HANG_NHANH_SHOP_ID = 4927242;
+const GIAO_HANG_NHANH_DEFAULT_WEIGHT = 200;
+const GIAO_HANG_NHANH_DEFAULT_SERVICE_ID = 53320;
+
+type GiaoHangNhanhFeeRequestParams = {
+    to_district_id: number;
+    to_ward_code: string;
+    cod_value: number;
+};
+type GiaoHangNhanhFeeData = {
+    total: number;
+    service_fee: number;
+    insurance_fee: number;
+    pick_station_fee: number;
+    coupon_value: number;
+    r2s_fee: number;
+    document_return: number;
+    double_check: number;
+    cod_fee: number;
+    pick_remote_areas_fee: number;
+    deliver_remote_areas_fee: number;
+    cod_failed_fee: number;
+};
+type GiaoHangNhanhFeeResponse = {
+    code: number;
+    message: string;
+    data: GiaoHangNhanhFeeData;
+};
+
+async function fetchGiaoHangNhanhWard(
+    params: GiaoHangNhanhFeeRequestParams
+): Promise<GiaoHangNhanhFeeData> {
+    const { data } = await axios.get<GiaoHangNhanhFeeResponse>(
+        "https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/fee",
+        {
+            headers: {
+                token: GIAO_HANG_HANH_API_TOKEN,
+            },
+            params: {
+                ...params,
+                shop_id: GIAO_HANG_NHANH_SHOP_ID,
+                weight: GIAO_HANG_NHANH_DEFAULT_WEIGHT,
+                service_id: GIAO_HANG_NHANH_DEFAULT_SERVICE_ID,
+                insurance_value: params.cod_value,
+            },
+        }
+    );
+    return data.data;
+}
 
 class GiaoHangNhanhService extends AbstractFulfillmentService {
     static identifier = "giao-hang-nhanh";
+    private logger: any;
 
     constructor(container, options) {
         super(container);
         // you can access options here
+        this.logger = container.logger;
 
         // you can also initialize a client that
         // communicates with a third-party service.
@@ -50,7 +104,20 @@ class GiaoHangNhanhService extends AbstractFulfillmentService {
         data: { [x: string]: unknown },
         cart: Cart
     ): Promise<number> {
-        return 123;
+        const { shipping_address } = cart;
+        const { address_2, province } = shipping_address;
+        let total = 123;
+        try {
+            const data = await fetchGiaoHangNhanhWard({
+                to_district_id: +province,
+                to_ward_code: address_2,
+                cod_value: cart.subtotal ?? 0,
+            });
+            total = +data.total;
+        } catch (e) {
+            this.logger.error(e);
+        }
+        return total;
     }
 
     async createFulfillment(
